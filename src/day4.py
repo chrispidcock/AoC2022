691a2b04917a08abf7aa8ee27413dbd29a7f24b4
chrispidcock <chris.pidcock87@gmail.com>
2023-06-26 13:07:32 +1000

    day4 part2 complete

--- src/day4.py
+++ src/day4.py
@@ -8,24 +8,46 @@ from src.funcs import file_manager
 input_strings = file_manager.read_file("day4.txt")
 
 
-def process_assignment_text(input: str) -> bool:
+def process_assignment_entire_overlap(input: str) -> bool:
     str1, str2 = input.split(',')
     rng1_min, rng1_max = str1.split('-')
     rng2_min, rng2_max = str2.split('-')
     if (int(rng1_min) <= int(rng2_min) and int(rng1_max) >= int(rng2_max)) \
         or (int(rng2_min) <= int(rng1_min) and int(rng2_max) >= int(rng1_max)):
-        return True
+            return True
+    return False
+
+
+def process_assignment_overlap(input: str) -> bool:
+    str1, str2 = input.split(',')
+    rng1_min, rng1_max = str1.split('-')
+    rng2_min, rng2_max = str2.split('-')
+    if (int(rng1_min) <= int(rng2_min) <= int(rng1_max)) \
+        or (int(rng2_min) <= int(rng1_min) <= int(rng2_max)):
+            return True
     return False
 
 
 def part1(input_list: list[str]) -> int:
     total = 0
     for entry in input_list:
-        total += 1 if process_assignment_text(entry) else 0
+        total += 1 if process_assignment_entire_overlap(entry) else 0
     return total
 
 
 part_1_total = part1(input_strings)
 
 print(f"{part_1_total=}")
+
+
+def part2(input_list: list[str]) -> int:
+    total = 0
+    for entry in input_list:
+        total += 1 if process_assignment_overlap(entry) else 0
+    return total
+
+
+part_2_total = part2(input_strings)
+
+print(f"{part_2_total=}")
 
--- src/tests/test_day4.py
+++ src/tests/test_day4.py
@@ -1,31 +1,68 @@
 from src import day4 as d4
 
 
-def test_process_assignment_text_end():
-    is_inclosed = d4.process_assignment_text("6-6,4-6")
+def test_process_assignment_entire_overlap_end():
+    is_inclosed = d4.process_assignment_entire_overlap("6-6,4-6")
     assert is_inclosed is True
 
 
-def test_process_assignment_text_single_enclosed():
-    is_inclosed = d4.process_assignment_text("6-6,4-7")
+def test_process_assignment_entire_overlap_single_enclosed():
+    is_inclosed = d4.process_assignment_entire_overlap("6-6,4-7")
     assert is_inclosed is True
 
 
-def test_process_assignment_text_double_enclosed():
-    is_inclosed = d4.process_assignment_text("6-7,4-9")
+def test_process_assignment_entire_overlap_double_enclosed():
+    is_inclosed = d4.process_assignment_entire_overlap("6-7,4-9")
     assert is_inclosed is True
 
 
-def test_process_assignment_text_large_overlap():
-    is_inclosed = d4.process_assignment_text("3-8,4-9")
+def test_process_assignment_entire_overlap_large_overlap():
+    is_inclosed = d4.process_assignment_entire_overlap("3-8,4-9")
     assert is_inclosed is False
 
 
-def test_process_assignment_text_single_overlap():
-    is_inclosed = d4.process_assignment_text("3-4,4-9")
+def test_process_assignment_entire_overlap_single_overlap():
+    is_inclosed = d4.process_assignment_entire_overlap("3-4,4-9")
     assert is_inclosed is False
 
 
-def test_process_assignment_text_no_overlap():
-    is_inclosed = d4.process_assignment_text("1-3,4-9")
+def test_process_assignment_entire_overlap_no_overlap():
+    is_inclosed = d4.process_assignment_entire_overlap("1-3,4-9")
     assert is_inclosed is False
+
+
+
+
+def test_process_assignment_overlap_end():
+    is_overlapping = d4.process_assignment_overlap("6-6,4-6")
+    assert is_overlapping is True
+
+
+def test_process_assignment_overlap_single_enclosed():
+    is_overlapping = d4.process_assignment_overlap("6-6,4-7")
+    assert is_overlapping is True
+
+
+def test_process_assignment_overlap_double_enclosed():
+    is_overlapping = d4.process_assignment_overlap("6-7,4-9")
+    assert is_overlapping is True
+
+
+def test_process_assignment_overlap_large_overlap():
+    is_overlapping = d4.process_assignment_overlap("3-8,4-9")
+    assert is_overlapping is True
+
+
+def test_process_assignment_overlap_single_overlap():
+    is_overlapping = d4.process_assignment_overlap("3-4,4-9")
+    assert is_overlapping is True
+
+
+def test_process_assignment_overlap_no_overlap():
+    is_overlapping = d4.process_assignment_overlap("1-3,4-9")
+    assert is_overlapping is False
+
+
+def test_process_assignment_overlap_no_overlap_swapped():
+    is_overlapping = d4.process_assignment_overlap("4-9,1-3")
+    assert is_overlapping is False
